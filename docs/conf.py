abc9fd64838235c1018e36cd133911786281f51a
Michiel Dubbelman <m.p.dubbelman@tudelft.nl>
2025-09-15 17:05:44 +0200

    Fixing sonar setup

--- docs/conf.py
+++ docs/conf.py
@@ -81,9 +81,9 @@ html_theme = "sphinx_rtd_theme"
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
     # Commonly used libraries, uncomment when used in package
-    # 'numpy': ('http://docs.scipy.org/doc/numpy/', None),
-    # 'scipy': ('http://docs.scipy.org/doc/scipy/reference/', None),
+    "numpy": ("http://docs.scipy.org/doc/numpy/", None),
+    "scipy": ("http://docs.scipy.org/doc/scipy/reference/", None),
     # 'scikit-learn': ('https://scikit-learn.org/stable/', None),
-    # 'matplotlib': ('https://matplotlib.org/stable/', None),
-    # 'pandas': ('http://pandas.pydata.org/docs/', None),
+    "matplotlib": ("https://matplotlib.org/stable/", None),
+    "pandas": ("http://pandas.pydata.org/docs/", None),
 }
